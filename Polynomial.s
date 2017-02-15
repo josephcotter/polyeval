cd8026f212ad575973ac188c627b0bd26977425f
Joseph Cotter <jcotter@scu.edu>
2017-02-15 00:54:14 -0800

    Add files via upload

---
+++ Polynomial.s
@@ -0,0 +1,25 @@
+        .syntax     unified
+        .cpu        cortex-m4
+        .text
+        .thumb_func
+        .align       2
+
+// float Polynomial(float x, float coef[], int32_t terms) ;
+
+        .global     Poly
+Poly:
+        VLDR        S1, zero    //S1 = result
+        VMOV        S2, 1.0     //S2 = power
+        LDR         R2, =0      //R2 = loopcount
+ loop:  VLDR        S3, [R0]    //S3 = current coef
+        VMUL.F32    S4, S2, S3  //S4 = power*coef
+        VADD.F32    S1, S1, S4  // S1 = result
+        VMUL.F32    S2, S2, S0  //power = power*x
+        ADD         R0, R0, 4   //moving to next element in coef[]
+        ADD         R2, R2, 1   //incrementing loopcount
+        CMP         R2, R1
+        BLT         loop        //break when loopcount = terms
+        VMOV        S0, S1
+        BX          LR
+zero:   .float      0.0
+
